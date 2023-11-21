819166b40a6f135aec8a2154321148b9e0795b62
frenzis01 <francesco.lorenzoni12042@gmail.com>
2023-11-21 13:13:06 +0100

    almost done implementing MSet

--- assignment1/ex3/MultiSet.hs
+++ assignment1/ex3/MultiSet.hs
@@ -1,2 +1,86 @@
-data MSet = MS [(a,Int)] deriving (Show)
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# HLINT ignore "Replace case with fromMaybe" #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+data MSet a = MS [(a,Int)] deriving (Show)
+
+-- instance Show a => Show (MSet a) where
+--   show (MS xs) = "MS " ++ show xs
+
+instance Eq a => Eq (MSet a) where
+   (==) :: Eq a => MSet a -> MSet a -> Bool
+   (MS a) == (MS b) =   all (\(x,m) -> occs (MS b) x == m) a &&
+                        all (\(x,m) -> occs (MS a) x == m) b
+
+-- Notice that foldr is applied only to the keys of the multiset, since
+-- it is defined as `Mset a` 
+instance Foldable MSet where
+   -- foldr :: (a -> b -> b) -> b -> MSet a -> b
+   foldr f z (MS []) = z
+   -- foldr f z (MS ((x,m):xs)) = foldr f (foldr f z (MS [(x,m)])) (MS xs)
+   -- foldr f z (MS ((x, m):xs)) = foldr f (f x (foldr f z (MS xs))) (MS [(x, m)])
+   -- foldr f z (MS xs) = foldr (\(x, m) acc -> f x acc) z xs -- This works!!
+   foldr f z (MS ((x, m):xs)) = f x (foldr f z (MS xs))
+
+callNTimes n f x
+  | n <= 0    = x
+  | otherwise = callNTimes (n - 1) f (f x)
+
+callNTimes' n f = foldl1 (.) (replicate n f)
+
+-- empty, that returns an empty MSet
+empty :: MSet a
+empty = MS []
+
+-- add mset v, returning a multiset obtained by adding the element v to mset.
+-- Clearly, if v is already present its multiplicity has to be increased by one, otherwise it
+-- has to be inserted with multiplicity 1.
+add :: Eq a => MSet a -> a -> MSet a
+add (MS mset) v = if v `notElem` elems (MS mset) then MS ((v,1):mset)
+   else MS [(x,if x == v then m + 1 else m) | (x,m) <- mset]
+
+(.+) :: Eq a => MSet a -> a -> MSet a
+(.+) = add
+
+-- occs mset v, returning the number of occurrences of v in mset (an Int).
+occs :: Eq a => MSet a -> a -> Int
+occs (MS mset) v = case lookup v mset of
+   Just mul -> mul
+   Nothing  -> 0
+
+-- elems mset, returning a list containing all the elements of mset.
+elems :: MSet a -> [a]
+elems (MS mset) = map fst mset
+
+-- subeq mset1 mset2, returning True if each element of mset1 is also an element
+-- of mset2 with the same multiplicity at least.
+
+-- subeq :: Eq a => MSet a -> MSet a -> Bool
+-- subeq :: Eq a => MSet (MSet (MSet a)) -> MSet a -> Bool
+subeq (MS mset1) (MS mset2) = all (\(x,mul) -> occs (MS mset2) x >= mul) mset1
+-- union mset1 mset2
+
+
+-- union mset1 mset2, returning an MSet having all the elements of mset1 and
+-- of mset2, each with the sum of the corresponding multiplicites.
+union :: Eq a => MSet a -> MSet a -> MSet a
+union (MS mset1) (MS mset2) =
+   MS ([(x,m + occs (MS mset2) x) | (x,m) <- mset1]
+    ++ filter (`notElem` mset1) mset2)
+(.++) :: Eq a => MSet a -> MSet a -> MSet a
+(.++) = union
+
+-- ms = add empty 1
+ms :: MSet Integer
+ms = empty .+ 1 .+ 1 .+ 2 .+ 3 .+ 5 .+ 5 .+ 1
+ms2 :: MSet Integer
+ms2 = empty .+ 2 .+ 1 .+ 2 .+ 5 .+ 3 .+ 5 .+ 1 .+ 1
+ms3 = callNTimes 6 (.+ 1) empty .++ callNTimes' 4 (.+ 12) empty .+ 5 .+ 3 .+ 5
+
+
+main = do
+   print ms
+   print ms3
+
 
