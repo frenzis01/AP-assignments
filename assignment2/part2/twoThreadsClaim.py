56df0cfa3c08b308f47bdd612029cb1a788ac571
frenzis01 <francesco.lorenzoni12042@gmail.com>
2023-12-24 15:28:30 +0100

    OK part2, aside from commnets, etc.

--- assignment2/part2/twoThreadsClaim.py
+++ assignment2/part2/twoThreadsClaim.py
@@ -2,6 +2,8 @@ import functools
 import time
 import threading
 import statistics as stats
+import json
+
 
 def bench (n_threads:int=1,seq_iter:int=1,iter:int=1):
    exec_times = []
@@ -49,12 +51,58 @@ def fib_run(n):
       a, b = b, a + b
    return a
 
-# When decorating with bench this, an infinite loop of thread creation starts 
+# When decorating this with bench, an infinite loop of thread creation starts 
 def fib_run_recursive(n):
     if n <= 1:
         return n
     else:
         return fib_run_recursive(n-1) + fib_run_recursive(n-2)
 
-result = fib_run(28)
-print(result)
+
+@bench(n_threads=2, seq_iter=8, iter=5)
+def rec_wrapper(func,*args,**kwargs):
+   return func(*args,**kwargs)
+
+def test(iter:int,fun:callable,args:tuple):
+
+   def write_bench_to_file(n_threads:int,seq_iter:int,iter:int):
+      res = bench(n_threads=n_threads, seq_iter=seq_iter, iter=iter)(fun)(*args)
+      filename = f"out/{fun.__name__}_{args}_{n_threads}_{seq_iter}.json"
+      print(f"flushing to {filename}")
+      with open(filename, 'w') as file:
+         file.write(str(json.dumps(res, indent=3)))
+      
+
+   # TEST 1    <16=seq_iter,1=n_threads>
+   write_bench_to_file(n_threads=1,seq_iter=16,iter=iter)
+   # redundant n_threads=1
+
+   # TEST 2    <8=seq_iter,2=n_threads>
+   write_bench_to_file(n_threads=2,seq_iter=8,iter=iter)
+   
+   # TEST 3    <4=seq_iter,4=n_threads>
+   write_bench_to_file(n_threads=4,seq_iter=4,iter=iter)
+   
+   # TEST 4    <2=seq_iter,8=n_threads>
+   write_bench_to_file(n_threads=8,seq_iter=2,iter=iter)
+
+def just_wait(n):
+   # NOOP for n/10 seconds
+   time.sleep(n * 0.1)
+
+def grezzo(n):
+   # CPU intensive
+   for i in range(2**n):
+      pass
+
+def main():
+   test(iter=5,fun=just_wait,args=(1,))
+   test(iter=5,fun=grezzo,args=(10,))
+
+   # result = fib_run(28)
+   # print(result)
+   # result = rec_wrapper(fib_run_recursive,28)
+   # print(result)
+
+if __name__ == "__main__":
+   main()
