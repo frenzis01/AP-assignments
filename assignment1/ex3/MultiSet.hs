bd7f4a1d95c0064a7979d4a3764228c6afd3254a
Francesco Lorenzoni <francesco.lorenzoni12042@gmail.com>
2024-07-26 10:10:20 +0200

    moved functions for better readibility

--- assignment1/ex3/MultiSet.hs
+++ assignment1/ex3/MultiSet.hs
@@ -21,24 +21,12 @@ import Data.List (foldl')
 
 data MSet a = MS [(a,Int)] deriving (Show)
 
+
 instance Eq a => Eq (MSet a) where
    (==) :: Eq a => MSet a -> MSet a -> Bool
    (MS a) == (MS b) =   all (\(x,m) -> occs (MS b) x == m) a &&
                         all (\(x,m) -> occs (MS a) x == m) b
 
--- diff was not requested by the assignment
--- it was used only for debugging purposes
--- 
--- | Given two MSet A,B finds all differences in the sets
---    @param A set to be compared
---    @param B set to be compared
---    @returns all keys which have a 
---             different multiplicity in A and B in a format like
---             [(key,A[key],B[key])] 
-
-diff :: Eq a => MSet a -> MSet a -> [(a, Int, Int)]
-diff (MS a) (MS b) =   [(x,m,mb) | (x,m) <- a, let mb = occs (MS b) x, m /= mb]
-                     ++ [(x,0,mb) | (x,mb) <- filter (\(x,m) -> x `notElem` map fst a) b]
 
 -- Notice that foldr is applied only to the keys of the multiset, since
 -- it is defined as `Mset a`. No operation on the multiplicities is performed
@@ -46,21 +34,11 @@ instance Foldable MSet where
    foldr f z (MS []) = z
    foldr f z (MS ((x, m):xs)) = f x (foldr f z (MS xs))
 
--- | callNTimes, applies n times f to x
---    @param f function to be applied
---    @param n number of times to apply f
---    @param x argument on which f should be appliead
-callNTimes :: (Ord t1, Num t1) => t1 -> (t2 -> t2) -> t2 -> t2
-callNTimes n f x
-  | n <= 0    = x
-  | otherwise = callNTimes (n - 1) f (f x)
-
-callNTimes' :: Int -> (c -> c) -> c -> c
-callNTimes' n f = foldl1 (.) (replicate n f)
 
 -- empty, that returns an empty MSet
 empty :: MSet a
 empty = MS []
+
 
 -- add mset v, returning a multiset obtained by adding the element v to mset.
 -- Clearly, if v is already present its multiplicity has to be increased by one, otherwise it
@@ -73,11 +51,6 @@ add (MS mset) v = if v `notElem` elems (MS mset) then MS ((v,1):mset)
 (.+) :: Eq a => MSet a -> a -> MSet a
 (.+) = add
 
--- Adds a list of keys to a MultiSet exploiting add.
--- useful only for readability purposes
-addList :: Eq a => MSet a -> [a] -> MSet a
-addList (MS mset) [] = MS mset
-addList (MS mset) (x:xs) = addList (add (MS mset) x) xs
 
 -- occs mset v, returning the number of occurrences of v in mset (an Int).
 occs :: Eq a => MSet a -> a -> Int
@@ -85,15 +58,6 @@ occs (MS mset) v = case lookup v mset of
    Just mul -> mul
    Nothing  -> 0
 
--- elems mset, returning a list containing all the elements of mset.
-elems :: MSet a -> [a]
-elems (MS mset) = map fst mset
-
--- subeq mset1 mset2, returning True if each element of mset1 is also an element
--- of mset2 with the same multiplicity at least.
-
-subeq :: Eq a => MSet a -> MSet a -> Bool
-subeq (MS mset1) (MS mset2) = all (\(x,mul) -> occs (MS mset2) x >= mul) mset1
 
 -- union mset1 mset2, returning an MSet having all the elements of mset1 and
 -- of mset2, each with the sum of the corresponding multiplicites.
@@ -105,21 +69,15 @@ union (MS mset1) (MS mset2) =
 (.++) = union
 
 
--- These two have to same behaviour of add and addList, but work on lists, not on MSet
--- They are needed for mapMSet
-addToMultiset :: (Eq a, Num b) => [(a, b)] -> a -> [(a, b)]
-addToMultiset mset v = if v `notElem` map fst mset then (v,1) : mset
-   else [(x,if x == v then m + 1 else m) | (x,m) <- mset]
+-- elems mset, returning a list containing all the elements of mset.
+elems :: MSet a -> [a]
+elems (MS mset) = map fst mset
 
--- These two addListToMultiset definitions are equal
--- inferred type signature differs a bit
-addListToMultiset :: (Eq a, Num b) => [(a, b)] -> [a] -> [(a, b)]
--- addListToMultiset l [] = l
--- addListToMultiset l (x:xs) = addListToMultiset (addToMultiset l x) xs
--- addListToMultiset :: [(Integer, Int)] -> [Integer] -> [(Integer, Int)]
-addListToMultiset = foldl addToMultiset
 
-mapMSet f (MS mset) = MS (foldl (\acc (v,n) -> addListToMultiset acc (replicate n (f v))) [] mset)
+-- subeq mset1 mset2, returning True if each element of mset1 is also an element
+-- of mset2 with the same multiplicity at least.
+subeq :: Eq a => MSet a -> MSet a -> Bool
+subeq (MS mset1) (MS mset2) = all (\(x,mul) -> occs (MS mset2) x >= mul) mset1
 
 {-
 -------------------------------------------------------------------------------------------------------
@@ -137,14 +95,68 @@ which does not allow to instantiate Functor, which requires the signature for fm
     fmap :: (a -> b) -> f a -> f b
 -}
 
+
+{-
+Below there are functions used for testing and debugging during development, 
+not requested by the assignment
+
+-}
+
+-- Given two MSet A,B finds all differences in the sets
+--    @param A set to be compared
+--    @param B set to be compared
+--    @returns all keys which have a 
+--             different multiplicity in A and B in a format like
+--             [(key,A[key],B[key])] 
+diff :: Eq a => MSet a -> MSet a -> [(a, Int, Int)]
+diff (MS a) (MS b) =   [(x,m,mb) | (x,m) <- a, let mb = occs (MS b) x, m /= mb]
+                     ++ [(x,0,mb) | (x,mb) <- filter (\(x,m) -> x `notElem` map fst a) b]
+
+-- callNTimes, applies n times f to x
+--    @param f function to be applied
+--    @param n number of times to apply f
+--    @param x argument on which f should be appliead
+callNTimes :: (Ord t1, Num t1) => t1 -> (t2 -> t2) -> t2 -> t2
+callNTimes n f x
+  | n <= 0    = x
+  | otherwise = callNTimes (n - 1) f (f x)
+
+callNTimes' :: Int -> (c -> c) -> c -> c
+callNTimes' n f = foldl1 (.) (replicate n f)
+
+-- Adds a list of keys to a MultiSet exploiting add.
+-- useful only for readability purposes
+addList :: Eq a => MSet a -> [a] -> MSet a
+addList (MS mset) [] = MS mset
+addList (MS mset) (x:xs) = addList (add (MS mset) x) xs
+
+
+-- These two have to same behaviour of add and addList, but work on lists, not on MSet
+-- They are needed for mapMSet
+addToMultiset :: (Eq a, Num b) => [(a, b)] -> a -> [(a, b)]
+addToMultiset mset v = if v `notElem` map fst mset then (v,1) : mset
+   else [(x,if x == v then m + 1 else m) | (x,m) <- mset]
+
+-- These two addListToMultiset definitions are equal
+-- inferred type signature differs a bit
+addListToMultiset :: (Eq a, Num b) => [(a, b)] -> [a] -> [(a, b)]
+-- addListToMultiset l [] = l
+-- addListToMultiset l (x:xs) = addListToMultiset (addToMultiset l x) xs
+-- addListToMultiset :: [(Integer, Int)] -> [Integer] -> [(Integer, Int)]
+addListToMultiset = foldl addToMultiset
+
+mapMSet :: Eq a => (t -> a) -> MSet t -> MSet a
+mapMSet f (MS mset) = MS (foldl (\acc (v,n) -> addListToMultiset acc (replicate n (f v))) [] mset)
+
+
 -- Basic test for debugging
--- ms = add empty 1
 ms :: MSet Integer
 ms = empty .+ 1 .+ 1 .+ 2 .+ 3 .+ 5 .+ 5 .+ 1
 ms2 :: MSet Integer
 ms2 = empty .+ 2 .+ 1 .+ 2 .+ 5 .+ 3 .+ 5 .+ 1 .+ 1
 ms3 = callNTimes 6 (.+ 1) empty .++ callNTimes' 4 (.+ 12) empty .+ 5 .+ 3 .+ 5
 
+basictest :: IO ()
 basictest = do
    print ms
    print ms3
@@ -155,6 +167,6 @@ basictest = do
    print ms4
    print $ mapMSet f (mapMSet g ms4) == mapMSet comp ms4
 
-   let exp_out = "\n---Expected output:\nMS [(5,2),(3,1),(2,1),(1,3)]\nMS [(3,1),(5,2),(1,6),(12,4)]\nMS [(24,4),(2,6),(10,2),(6,1)]\nTrue"
+   let exp_out = "\n---Expected output:\nMS [(5,2),(3,1),(2,1),(1,3)]\nMS [(3,1),(5,2),(1,6),(12,4)]\nMS [(24,4),(2,6),(10,2),(6,1)]\nTrue\n"
    -- putStr to evaluate the \n
    putStr exp_out
